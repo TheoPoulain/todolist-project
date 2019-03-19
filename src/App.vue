c25419fbe8caaec4f805810a8b389d3da4c32498
Théo Poulain <theofb92@gmail.com>
2019-03-19 16:29:05 +0100

    second commit

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+    <div id="app" class="container">
+        <img src="./assets/logo.png" class="logo">
+        
+        <Todos></Todos>
+        <CurrentTodos></CurrentTodos>
+        <CompletedTodos></CompletedTodos>
+
+        <!-- <router-view/> -->
+    </div>
+</template>
+
+<script>
+    import Todos from './components/Todos.vue'
+    import CurrentTodos from './components/CurrentTodos.vue'
+    import CompletedTodos from './components/CompletedTodos.vue'
+
+    import 'bootstrap/dist/css/bootstrap.css'
+    import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+    export default {
+        name: 'App',
+        components: {
+          Todos,
+          CurrentTodos,
+          CompletedTodos
+        }
+    }
+</script>
+
+<style>
+
+* {
+  box-sizing: border-box;
+}
+
+.container {
+  max-width: 600px;
+  margin: 0 auto;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto;
+  height: 100px;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-size: 20px;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,49 @@
+<template>
+    <div id="todos" class="container">
+        <h1>Todo List</h1>
+        
+        <div class="todo-list">
+            <input class="form-control" :value="newTodo" @change="getTodo" @keyup.enter="addTodo" placeholder="New Todo ...">
+            <button class="btn btn-primary" @click="addTodo">Add</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    import api from '@/api'
+    import { mapMutations } from 'vuex'
+    
+    export default{
+        data() {
+            return {
+                todos: {}
+            }
+        },
+        methods: {
+            getTodo(e){
+                this.$store.dispatch('getTodo', e.target.value)
+            },
+            addTodo(){
+                this.$store.dispatch('addTodo')
+                this.$store.dispatch('clearTodo')
+            }
+        },
+        computed: {
+            newTodo(){
+                return this.$store.getters.newTodo
+            }
+        }
+    }
+</script>
+
+<style>
+    #todos{
+        margin-top: 50px;
+    }
+    .todo-list {
+        margin: 5px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
+</style>
